4a284c5876d6760096bb766909bf7e547facc266
Baptiste Mikaelian <baptiste.mikaelian@gmail.com>
2014-11-19 13:44:21 +0100

    Push Sillex

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo "coucou"; ?>
+<?php 
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
+ ?>
