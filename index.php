53e7b77d36cf00f63b1aea37b19a64fd8133018b
Baptiste Mikaelian <baptiste.mikaelian@gmail.com>
2014-11-19 14:34:10 +0100

    Ajout du fichier vue et ajout de twig

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php 
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/', function () use ($app){
+    return $app['twig']->render('root.twig', array());
+});
+
+
+$app->post('/get', function (Request $request) use ($app) {
+    $filename = $request->get('filename');
+    exec('subliminal -l fr -- ' . $filename);
+    if (!file_exists('/tmp/subtitle/' . $filename)) {
+        $app->abort(404);
+    }
+    return $app->sendFile('/tmp/subtitle/' . $filename);
 });
 
 $app->run();
